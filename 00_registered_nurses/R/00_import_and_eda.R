d1f9543991a52a9a78f6a393c47a9e551da5b379
Jesse Mostipak <jesse.maegan@gmail.com>
2021-10-06 14:50:28 -0500

    registered nurses initial commit

---
+++ 00_registered_nurses/R/00_import_and_eda.R
@@ -0,0 +1,10 @@
+# setup ----
+library(tidyverse)
+
+# data import ----
+nurses <- read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2021/2021-10-05/nurses.csv')
+
+# initial eda ----
+
+
+# building some things ----
---
+++ 01_app/app.R
@@ -0,0 +1,15 @@
+# set up ----
+library(shiny)
+
+# ui ----
+ui <- fluidPage(
+  
+)
+
+# server ----
+server <- function(input, output, session) {
+  
+}
+
+# run app ----
+shinyApp(ui, server)
