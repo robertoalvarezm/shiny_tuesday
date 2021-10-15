1312d6b926ddfa58abcbdd901ab0d9c3eaabbd17
Jesse Mostipak <jesse.maegan@gmail.com>
2021-10-15 17:00:16 -0500

    ggiraph working with line graph

--- 01_global_seafood_prod/app.R
+++ 01_global_seafood_prod/app.R
@@ -1,7 +1,11 @@
 # set up ----
 library(shiny)
 library(ggplot2)
+library(dplyr)
 library(readr)
+library(thematic)
+library(ggiraph)
+library(bslib)
 library(here)
 
 # data import ----
@@ -10,18 +14,32 @@ tidy_fish <- read_csv(here("01_global_seafood_prod", "tidy_fish.csv"))
 # user interface ----
 ui <- fluidPage(
   
+  theme = bs_theme(version = 4,
+                   bootswatch = "minty"),
+  
   sidebarLayout(
     
     ## sidebar panel ----
     sidebarPanel(
       
+      tags$strong("Inputs for the bar plot"),
+      ## bar plot  
       ### year input ----
+      
       selectizeInput(inputId = "year",
                      label = "Select a year",
                      #' because there are duplicate years, need to pull out
                      #' unique years only in order for selected to work
                      choices = unique(tidy_fish$year), 
-                     selected = "1950") 
+                     selected = "1950"),  
+      
+      ## line graph
+      ### sector catch type
+      tags$strong("Inputs for the line graph"),
+      checkboxGroupInput(inputId = "sector_catch_type",
+                         label = "Select a type",
+                         choices = unique(tidy_fish$sector_catch_type),
+                         selected = unique(tidy_fish$sector_catch_type))
     ),
     
     ## main panel ----
@@ -31,9 +49,14 @@ ui <- fluidPage(
       plotOutput(outputId = "sector_type_barplot"),
       
       ### sector type line graph (ui)
-      plotOutput(outputId = "sector_type_line_graph",
-                 click = "plot_click"),
+      # # ggplot
+      # plotOutput(outputId = "sector_type_line_graph",
+      #            brush = "plot_brush"),
       
+      # girafe
+      girafeOutput("giraffe_plot"),
+      
+      # table from brushed lines in ggplot line graph
       tableOutput(outputId = "table_data")
     )
   )
@@ -47,6 +70,10 @@ server <- function(input, output, session) {
     tidy_fish %>% filter(year %in% input$year)
   })
   
+  reactive_fish_table <- reactive({
+    tidy_fish %>% filter(sector_catch_type %in% input$sector_catch_type)
+  })
+  
   ## sector type barplot (server) ----
   output$sector_type_barplot <- renderPlot({
     
@@ -54,27 +81,55 @@ server <- function(input, output, session) {
     
     ggplot(data = reactive_fish(),
            aes(x = sector_catch_type, y = value)) + 
-      geom_col()
+      geom_col() + 
+      theme_bw()
     
   }, res = 96)
   
   ## sector type line graph (server) ----
-  output$sector_type_line_graph <- renderPlot({
+  ### plot ----
+  # output$sector_type_line_graph <- renderPlot({
+  #   
+  #   req(input$year)
+  #   
+  #   ggplot(data = reactive_fish_table(),
+  #          aes(x = year, y = value, group = sector_catch_type)) +
+  #     geom_line(aes(color = sector_catch_type),
+  #               size = 1.5) + 
+  #     theme_bw() +
+  #     theme(legend.position = "none")
+  #   
+  # }, res = 96)
+  
+  ### now in giraffe flavor ----
+  
+  output$giraffe_plot <- renderGirafe({
     
     req(input$year)
     
-    ggplot(data = tidy_fish,
-           aes(x = year, y = value, group = sector_catch_type)) +
-      geom_line(aes(color = sector_catch_type))
-  }, res = 96)
+    gg_line <- ggplot(reactive_fish_table(),
+                   aes(x = year, y = value, group = sector_catch_type,
+                       tooltip = paste(sector_catch_type, value, year, sep = "\n"), 
+                       data_id = NULL,
+                       color = sector_catch_type)) +
+      geom_line_interactive(aes(color = sector_catch_type),
+                            size = 1.5) +
+      theme_bw() +
+      theme(legend.position = "none")
+    
+    girafe(ggobj = gg_line,
+           options = list(opts_selection(type = "multiple", only_shiny = FALSE)))
+  })
   
   ## sector type data table (server) ----
   output$table_data <- renderTable({
-    nearPoints(tidy_fish,
-               input$plot_click,
-               xvar = "year", yvar = "value")
+    
+    brushedPoints(tidy_fish,
+                  input$plot_brush)
+
   })
 }
 
 # RUN THE APP! ----
+thematic_shiny()
 shinyApp(ui, server)
